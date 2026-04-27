40a47c3932533f69faec675afaf3e3c7770771fd
gndusdl719 <gndusdl719@gmail.com>
2026-04-27 17:58:18 +0900

    2026.04.27 자바스크립트 연습

--- js/test.js
+++
@@ -1,8 +0,0 @@
-let kor = 90;
-let usa = 70;
-let mat = 78;
-let tot = kor + usa + mat; //등호가 나오면 등호는 맨 마지막
-let too = tot / 3; //등호가 나오면 등호는 맨 마지막
-console.log(tot);
-console.log(too);
-// console.log(tot / 3);
